3b193143e510da918bfa5884b72467c1896ead2d
gideon-del <gideonchidi471@gmail.com>
2023-07-06 21:13:13 +0100

    Hook
    Practicing hooks

---
+++ src/hooks/useThumbnaill.js
@@ -0,0 +1,5 @@
+import img1 from "../assets/image-product-1.jpg";
+import img2 from "../assets/image-product-2.jpg";
+import img3 from "../assets/image-product-3.jpg";
+import img4 from "../assets/image-product-4.jpg";
+const useThumnail = () => {};

9cd9ca7e8ccd5ee105233842542fce6945afa9c1
gideon-del <gideonchidi471@gmail.com>
2023-07-14 21:10:31 +0100

    Cart
    Adding cart logic

---
+++ src/hooks/useCart.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function useCart() {
+  const [cart, setCart] = useState(null);
+  const changeQuantity = ({ price, num }) => {
+    if (num <= 0) {
+      setCart(null);
+      return;
+    }
+    setCart({ quantity: num, price });
+  };
+  return { cart, changeQuantity };
+}
+
+export const { cart, changeQuantity } = useCart();
